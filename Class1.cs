de33813511d236083a0a0b6c75d45293cf7a0fbc
Dober09 <lebohanglehutjo@gmail.com>
2024-06-13 14:16:05 +0200

    first commit

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public abstract class Area
+    {
+
+        public double getArea(int a)
+        {
+            return 0;
+        }
+
+    }
+
+    public class Tripezium: Area
+    {
+
+        public double getArea(double a, double b, double h)
+        {
+
+            return 0.5 * (a + b) * h;
+
+        }
+
+    }
+
+    public class Square : Area
+    {
+
+        public double getArea(double s)
+        {
+
+            return s*s;
+
+        }
+
+    }
+
+    public class Triangle : Area
+    {
+
+        public double getArea(double b, double h)
+        {
+
+            return 0.5 * b * h;
+
+        }
+
+    }
+
+    public class Circle : Area
+    {
+        double pi = Math.PI;
+        public double getArea(double r)
+        {
+
+            return pi * r*r;
+
+        }
+
+    }
+
+    public class Rectangle : Area
+    {
+        public double getArea(double l, double w)
+        {
+
+            return  l * w;
+
+        }
+
+    }
+
+}
