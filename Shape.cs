3b50943b119b00e2c42f98acf1c0315e49bc1ba5
Dober09 <lebohanglehutjo@gmail.com>
2024-06-14 09:06:36 +0200

    renamed and modified class1 to shape

---
+++ Shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    
+    public abstract class Shape
+    {
+        public const double PI = Math.PI;  
+        private double w { get; set; }
+        private double x {  get; set; }
+        private double y { get; set; }
+        private double z { get; set; }
+        public Shape() : this(0, 0, 0, 0) { }
+
+        public Shape(double w, double x, double y, double z) {
+            this.w = w;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public abstract double Area();
+
+        public virtual double SurfaceArea()
+        {
+            return 0;
+        }
+    }
+
+}
